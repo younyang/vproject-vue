7ad540d2e1115de010a61a17fcb65d677fe06cc2
younjinkim <younyang80@gmail.com>
2018-06-07 18:48:30 +0900

    menu group

--- src/views/settings/MenuGroup.vue
+++ src/views/settings/MenuGroup.vue
@@ -2,7 +2,7 @@
   <div class="animated fadeIn">
     <div class="row">
       <div class="col-6">
-        <!-- Menu Tree -->
+        <!-- Menu Group Tree -->
         <div class="tree-area">
           <div class="tree-btn">
             <multiselect
@@ -28,214 +28,81 @@
             :data="menus"
             :whole-row="true"
             :collapse="true"
+            show-checkbox
+            multiple
+            allow-batch
             text-field-name="menuName"
             value-field-name="menuId"
             children-field-name="subMenuList"
             @item-click="onItemClick">
             <template slot-scope="_">
-              <div>
+              <div style="display: inherit">
                 <i :class="_.vm.themeIconClasses" role="presentation" v-if="!_.model.loading"></i>
-                {{ _.model.menuPermissionCode }}
                 {{_.model.menuName}}
-                <span v-if="_.model.menuId !== 1">
+                <span class="menu-btn" v-if="_.model.menuId !== 1">
                   <b-button
                     type="button"
-                    data-id="read"
                     :variant="_.model.menuPermissionCode === 'MENU_PERMISSION_01' ? 'primary' : 'outline-secondary'"
-                    @click="onSetAuth(_.model)"
+                    @click="onSetAuth(_.model, '1')"
                   >읽기</b-button>
                   <b-button
                     type="button"
-                    data-id="read"
                     :variant="_.model.menuPermissionCode === 'MENU_PERMISSION_02' ? 'primary' : 'outline-secondary'"
-                    @click="onSetAuth(_.model)"
+                    @click="onSetAuth(_.model, '2')"
                   >쓰기</b-button>
                 </span>
               </div>
             </template>
           </v-jstree>
         </div>
+        <div class="page-btn">
+          <b-button type="button" variant="outline-secondary" @click="onRemoveItem">삭제</b-button>
+        </div>
       </div>
       <div class="col-6">
-        <div class="collapse-title">
-          <b-button class="btn-collapse" v-b-toggle.formDefault>
-            <i class="fa"></i>
-            기본정보
-          </b-button>
+        <!-- Menu All Tree -->
+        <div class="tree-area">
+          <div class="tree-btn">
+            <strong class="title">전체 메뉴</strong>
+            <b-button type="button" variant="in-table" @click="onExpandAll">모두 열기</b-button>
+            <b-button type="button" variant="in-table" @click="onCollapseAll">모두 닫기</b-button>
+          </div>
+
+          <v-jstree
+            ref="tree2"
+            :data="menus"
+            :whole-row="true"
+            :collapse="true"
+            show-checkbox
+            multiple
+            allow-batch
+            text-field-name="menuName"
+            value-field-name="menuId"
+            children-field-name="subMenuList"
+            @item-click="onItemClick">
+            <template slot-scope="_">
+              <div style="display: inherit">
+                <i :class="_.vm.themeIconClasses" role="presentation" v-if="!_.model.loading"></i>
+                {{_.model.menuName}}
+                <span class="menu-btn" v-if="_.model.menuId !== 1">
+                  <b-button
+                    type="button"
+                    :variant="_.model.menuPermissionCode === 'MENU_PERMISSION_01' ? 'primary' : 'outline-secondary'"
+                    @click="onSetAuth(_.model, '1')"
+                  >읽기</b-button>
+                  <b-button
+                    type="button"
+                    :variant="_.model.menuPermissionCode === 'MENU_PERMISSION_02' ? 'primary' : 'outline-secondary'"
+                    @click="onSetAuth(_.model, '2')"
+                  >쓰기</b-button>
+                </span>
+              </div>
+            </template>
+          </v-jstree>
         </div>
 
-        <b-collapse id="formDefault" visible>
-          <b-form class="formView" :validated="inValidForm" novalidate>
-            <input type="hidden" :value="items.menuId">
-
-            <!-- 메뉴명 -->
-            <b-form-fieldset
-              :invalid-feedback="$valid.msg.require"
-              :horizontal="true">
-              <template slot="label">
-                메뉴명<i v-if="isEdit" class="require">*</i>
-              </template>
-              <b-form-input
-                v-model="items.menuName"
-                type="text"
-                :plaintext="!isEdit"
-                required
-              ></b-form-input>
-            </b-form-fieldset>
-
-            <!-- 링크 URL -->
-            <b-form-fieldset
-              :invalid-feedback="$valid.msg.require"
-              :horizontal="true">
-              <template slot="label">
-                링크 URL<i v-if="isEdit" class="require">*</i>
-              </template>
-              <b-form-input
-                v-model="items.linkUrl"
-                type="text"
-                :plaintext="!isEdit"
-                required
-              ></b-form-input>
-            </b-form-fieldset>
-
-            <!-- 메뉴권한설정 -->
-            <b-form-fieldset
-              :invalid-feedback="$valid.msg.select"
-              :horizontal="true">
-              <template slot="label">
-                메뉴권한설정<i v-if="isEdit" class="require">*</i>
-              </template>
-              <b-form-radio-group
-                v-if="isEdit"
-                :options="code.menuPermissionCode"
-                v-model="items.menuPermissionCode"
-              ></b-form-radio-group>
-              <b-form-input
-                v-else
-                :value="menuPermissionCodeText"
-                type="text"
-                plaintext
-              ></b-form-input>
-            </b-form-fieldset>
-
-            <!-- 노출여부 -->
-            <b-form-fieldset
-              label="노출여부"
-              :horizontal="true">
-              <c-switch
-                v-if="isEdit"
-                type="text"
-                class="v-switch"
-                on="노출"
-                off="미노출"
-                v-model="items.menuExposureYn"
-              ></c-switch>
-              <span
-                v-else
-                class="badge"
-                :class="{'primary' : items.menuExposureYn }"
-              >{{ items.menuId !== 1 ? items.menuExposureYn ? '사용' : '미사용' : '--' }}
-              </span>
-            </b-form-fieldset>
-
-            <!-- 사용여부 -->
-            <b-form-fieldset
-              label="사용여부"
-              :horizontal="true">
-              <c-switch
-                v-if="isEdit"
-                type="text"
-                class="v-switch"
-                on="사용"
-                off="미사용"
-                v-model="items.menuUseYn"
-              ></c-switch>
-              <span
-                v-else
-                class="badge"
-                :class="{'primary' : items.menuUseYn }"
-              >{{ items.menuId !== 1 ? items.menuUseYn ? '사용' : '미사용' : '--' }}
-              </span>
-            </b-form-fieldset>
-
-            <!-- 설명 -->
-            <b-form-fieldset
-              label="설명"
-              :horizontal="true">
-              <b-form-textarea
-                v-model="items.menuDesc"
-                :rows="6"
-                :no-resize="true"
-                :plaintext="!isEdit"
-              ></b-form-textarea>
-            </b-form-fieldset>
-          </b-form>
-        </b-collapse>
-
-        <!-- 처리이력 -->
-        <div class="collapse-title" v-if="!isEdit">
-          <b-button class="btn-collapse" v-b-toggle.formHistory>
-            <i class="fa"></i>
-            처리이력
-          </b-button>
-        </div>
-        <b-collapse id="formHistory" visible v-if="!isEdit">
-          <b-form class="formView">
-            <div class="form-row">
-              <!-- 등록일 -->
-              <b-form-fieldset
-                label="등록일시"
-                :horizontal="true">
-                <b-form-input
-                  :value="items.createDateTime"
-                  plaintext
-                  type="text"
-                ></b-form-input>
-              </b-form-fieldset>
-              <!-- 등록자 -->
-              <b-form-fieldset
-                label="등록자"
-                :horizontal="true">
-                <b-form-input
-                  :value="items.createId"
-                  plaintext
-                  type="text"
-                ></b-form-input>
-              </b-form-fieldset>
-            </div>
-
-            <div class="form-row">
-              <!-- 수정일 -->
-              <b-form-fieldset
-                label="수정일시"
-                :horizontal="true">
-                <b-form-input
-                  :value="items.modifyDateTime"
-                  plaintext
-                  type="text"
-                ></b-form-input>
-              </b-form-fieldset>
-              <!-- 수정자 -->
-              <b-form-fieldset
-                label="수정자"
-                :horizontal="true">
-                <b-form-input
-                  :value="items.modifyId"
-                  plaintext
-                  type="text"
-                ></b-form-input>
-              </b-form-fieldset>
-            </div>
-          </b-form>
-        </b-collapse>
-
-        <div class="page-btn" v-if="isEdit && items.menuId !== 1">
-          <b-button type="button" variant="outline-secondary" @click="onCancel">취소</b-button>
-          <b-button type="button" variant="primary" @click="onSubmit">저장</b-button>
-        </div>
-
-        <div class="page-btn" v-if="!isEdit && items.menuId !== 1">
-          <b-button type="button" variant="primary" @click="onEdit">수정</b-button>
+        <div class="page-btn">
+          <b-button type="button" variant="primary" @click="onAddItem">추가</b-button>
         </div>
       </div>
     </div>
@@ -273,18 +140,6 @@
         groups: [],
         menuGroups: [],
         nodes: false,
-        items: {
-          menuId: 1,
-          menuName : null,
-          linkUrl : null,
-          menuPermissionCode : null,
-          menuExposureYn : false,
-          menuDesc : null,
-          menuLevel : null,
-          menuUseYn : false,
-          criteriaMenuId : null,
-          criteriaMenuLevel : null
-        },
 
         code: {
           groups: [],
@@ -300,11 +155,7 @@
           type: 'done',
           msg: '',
           action (){}
-        },
-
-        isEdit: false,
-        isCreate: false,
-        inValidForm: false
+        }
       }
     },
 
@@ -315,7 +166,7 @@
       }
     },
 
-    created (){
+    mounted (){
       // Groups
       this.$https.get('/setting/admin/groups')
         .then((res) => {
@@ -345,19 +196,14 @@
           .then(res => {
             console.log(res.data.items)
           });
-
       },
 
-      fetchMenu (id){
-        const uri = id ? `/setting/admin/menu/${id}` : `/setting/admin/menu`;
-        const action = id ? this.onFetchItem : (res) => {
-          if (this.menus.length) {
-            this.$eventBus.$emit('menu', [res.data.items]);
-          }else{
-            this.onFetchList([res.data.items]);
-          }
-        };
-        this.$https.get(uri).then(action);
+      fetchMenu (){
+        this.$https.get('/setting/admin/menu')
+          .then(res => {
+            this.menus = [res.data.items];
+            this.$refs.tree.initializeData(this.menus);
+          });
       },
 
       onChangeGroup (item){
@@ -365,98 +211,6 @@
         this.fetchMenuGroup(id);
       },
 
-      onFetchList (nav){
-        this.menus = nav;
-        this.$refs.tree.initializeData(this.menus);
-        this.$refs.tree.onItemDrop = this.onItemDrop;
-        setTimeout(() => {
-          const node = this.$refs.tree.$children[0];
-          this.setSelectedMenu(node);
-        },0);
-      },
-
-      onFetchItem (res){
-        this.items = { ...res.data.items };
-        this.nodes.model = Object.assign(this.nodes.model, res.data.items);
-        this.originItems = JSON.parse(JSON.stringify(this.items));
-      },
-
-      onEdit (){
-        this.isEdit = true;
-        this.isCreate = false;
-      },
-
-      onView (noChange = true){
-        this.isEdit = false;
-        this.isCreate = false;
-        if (noChange){
-          this.items = JSON.parse(JSON.stringify(this.originItems));
-        }
-      },
-
-      onCancel (){
-        if (this.items.menuId === 'add'){
-          this.onRemoveItem();
-        }else{
-          this.onView();
-        }
-      },
-
-      onSubmit (){
-        const {
-          menuId,
-          menuName,
-          linkUrl,
-          menuPermissionCode,
-          menuExposureYn,
-          menuDesc,
-          menuLevel,
-          menuUseYn,
-          criteriaMenuId,
-          criteriaMenuLevel
-        } = this.items;
-
-        const submitItems = this.isCreate ? {
-          menuName,
-          linkUrl,
-          menuPermissionCode,
-          menuExposureYn,
-          menuDesc,
-          menuLevel,
-          menuUseYn,
-          criteriaMenuId,
-          criteriaMenuLevel
-        } : {
-          menuName,
-          linkUrl,
-          menuPermissionCode,
-          menuExposureYn,
-          menuDesc,
-          menuUseYn
-        };
-
-        const validate = this.$valid.all(submitItems);
-        const submitAction = (this.isCreate) ?
-          () => this.$https.post(`/setting/admin/menu`, submitItems) :
-          () => this.$https.put(`/setting/admin/menu/${menuId}`, submitItems);
-
-
-        this.inValidForm = !validate;
-
-        if (validate) {
-          submitAction()
-            .then((res) => {
-              const id = this.isCreate ? res.data.items : menuId;
-              this.onView(false);
-              this.modal.open = false;
-              this.fetchMenu();
-              this.fetchMenu(id);
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        }
-      },
 
       onDeleteData (){
         this.items.criteriaMenuId = this.items.criteriaMenuId || 26
@@ -468,60 +222,13 @@
           });
       },
 
+      onSetAuth (obj, code){
+        //obj.menuPermissionCode = `MENU_PERMISSION_0${code}`;
+      },
+
       onItemClick (node) {
-        if (this.items.menuId === 'add' && node.model.menuId !== 'add'){
-          this.items.menuId = node.model.menuId;
-          this.setRemoveMenu();
-          return;
-        }
-
         this.nodes = node;
-        if (node.model.menuId !== 'add'){
-          this.fetchMenu(node.model.menuId);
-          return;
-        }
-        this.isEdit = true;
-        this.isCreate = true;
       },
-
-      onSetAuth (e){
-        console.log(e);
-      },
-
-      onItemDrop (e, oriNode, oriItem) {
-        const tree = this.$refs.tree;
-        if (!tree.draggable || oriItem.dropDisabled) {
-          return false;
-        }
-        if (!tree.draggedElm || tree.draggedElm === e.target || tree.draggedElm.contains(e.target)) {
-          return;
-        }
-        if (tree.draggedItem) {
-          console.log(oriItem);
-          console.log(tree.draggedItem)
-        }
-        /*
-         if (this.draggedItem.parentItem === oriItem[this.childrenFieldName]
-         || this.draggedItem.item === oriItem
-         || (oriItem[this.childrenFieldName] && oriItem[this.childrenFieldName].findIndex(t => t.id === this.draggedItem.item.id) !== -1)) {
-         return;
-         }
-         if (!!oriItem[this.childrenFieldName]) {
-         oriItem[this.childrenFieldName].push(this.draggedItem.item)
-         } else {
-         oriItem[this.childrenFieldName] = [this.draggedItem.item]
-         }
-         oriItem.opened = true
-         var draggedItem = this.draggedItem
-         this.$nextTick(() => {
-         draggedItem.parentItem.splice(draggedItem.index, 1)
-         })
-         this.$emit("item-drop", oriNode, oriItem, draggedItem.item, e)
-         */
-
-
-      },
-
 
       onAddItem (){
         const { menuId, menuLevel } = this.items;
@@ -588,20 +295,6 @@
         if (firstItem) {
           firstItem.model.closeChildren();
         }
-      },
-
-      setSelectedMenu (node){
-        const { model } = node;
-        this.$refs.tree.handleSingleSelectItems(node, model);
-        this.onItemClick(node);
-      },
-
-      setRemoveMenu (){
-        const currentIndex = this.nodes.parentItem.indexOf(this.nodes.model);
-        this.nodes.parentItem.splice(currentIndex, 1);
-        this.onView(false);
-        this.modal.open = false;
-        this.setSelectedMenu(this.nodes.$parent);
       }
     }
   }
